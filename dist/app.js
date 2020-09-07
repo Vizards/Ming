15faa439b5235a9800a00da04a812218ac6d41cd
Vizards <gvizards@gmail.com>
2020-09-07 14:50:14 +0800

    feat: add example dist folder

---
+++ dist/app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const Hapi = require('@hapi/hapi')
+const Inert = require('inert')
+
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: '0.0.0.0'
+})
+
+const start = async () => {
+  await server.register(Inert)
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.file('index.html')
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/{micro_app}/{path*}',
+    handler: (request, h) => {
+      const micro_app = request.params.micro_app;
+      const path = request.params.path;
+
+      /* {domain}/*.[js|css] */
+      if (micro_app.includes('.')) { return h.file(micro_app) }
+      /* {domain}/{micro_app} */
+      if (!micro_app.includes('.') && !path) { return h.file('index.html') }
+      /* {domain}/{micro_app}/{path*} */
+      if (!micro_app.includes('.') && path && !path.includes('.')) { return h.file('index.html') }
+      /* {domain}/{micro_app}/*.[js|css] */
+      if (!micro_app.includes('.') && path && path.includes('.')) { return h.file(`${micro_app}/${path}`) }
+      /* 404 fallback to foundation app */
+      return h.file('index.html')
+    }
+  })
+
+  await server.start()
+
+  console.log('Server running at:', server.info.uri)
+}
+
+start()
